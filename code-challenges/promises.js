d7e05627009dda5b19e5508abb14844d9486555a
alex kazanski <alex1kazanski@gmail.com>
2020-09-13 08:51:06 -0400

    add(code-challenge): promise vs callback

---
+++ code-challenges/promises.js
@@ -0,0 +1,29 @@
+// callbacks
+const doWorkCallback = (callback) => {
+  setTimeout(() => {
+    // callback("This is my error!", undefined);
+    callback(undefined, [1, 4, 7]);
+  }, 2000);
+  return;
+};
+
+doWorkCallback((error, result) => {
+  if (error) return console.log(error);
+  console.log(result);
+});
+
+// promises
+const doWorkPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    //resolve([7, 4, 1]);
+    reject("Things went wrong!");
+  }, 2000);
+});
+
+doWorkPromise
+  .then((result) => {
+    console.log("Success!", result);
+  })
+  .catch((error) => {
+    console.log("Error!", error);
+  });
