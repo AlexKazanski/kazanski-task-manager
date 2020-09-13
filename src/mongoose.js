7b1208f5b205f7f272188359d78ab14266b05a53
alex kazanski <alex1kazanski@gmail.com>
2020-09-13 13:49:18 -0400

    feat(mongoose): adding models

--- src/mongoose.js
+++ src/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const databaseName = "task-manager-api";
 const connectionURL = `mongodb://root:password123@127.0.0.1:27017/${databaseName}`;
@@ -12,15 +13,42 @@ mongoose.connect(connectionURL, {
 const User = mongoose.model("User", {
   name: {
     type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) throw new Error("Email is invalid!");
+    },
+  },
+  password: {
+    type: String,
+    min: 7,
+    required: true,
+    trim: true,
+    validate(value) {
+      if (value.includes("password"))
+        throw new Error("Password cant contain 'password'!");
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) throw new Error("Age must be a positive number!");
+    },
   },
 });
 
 const me = new User({
-  name: "Alexander",
-  age: '30alpha',
+  name: "     Alexander James Kazanski        ",
+  age: 28,
+  email: "TESTALPHA@GMAIL.COM",
+  password: "arandomhashwithalonglength",
 });
 
 me.save()
@@ -29,4 +57,27 @@ me.save()
   })
   .catch((error) => {
     console.log("Error!", error);
+  });
+
+const Task = mongoose.model("Task", {
+  description: {
+    type: String,
+    trim: true,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+new Task({
+  description: "  This is the description   ",
+})
+  .save()
+  .then((task) => {
+    console.log(task);
+  })
+  .catch((error) => {
+    console.log(error);
   });
